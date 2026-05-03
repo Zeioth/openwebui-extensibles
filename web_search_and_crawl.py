5ed2e50b3088ea592b9706077f68349ca3ec94b7
Zeioth <Zeioth@hotmail.com>
2026-05-03 02:46:12 +0200

    Update web_search_and_crawl.py

--- web_search_and_crawl.py
+++ web_search_and_crawl.py
@@ -4,7 +4,7 @@ description: Search and Crawls the web using SearXNG, OpenWebUI Native Search, a
 author: lexiismadd, zeioth
 author_url: https://github.com/lexiismad, https://github.com/zeioth
 funding_url: https://github.com/open-webui
-version: 2.8.8
+version: 2.8.10
 license: MIT
 requirements: aiohttp, loguru, crawl4ai, orjson, tiktoken
 """
